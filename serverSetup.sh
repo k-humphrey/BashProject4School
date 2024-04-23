33d0ef4f2bb7ae97d1df52a8584eb19bdf4a1021
Kayla Humphrey <kl1humphrey@gmail.com>
2024-04-23 23:34:49 +0000

    init commit for serverSetup.sh

---
+++ serverSetup.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+# Description: a server manager that will create servers, set them up, and log their information
+# Creator: Kayla Humphrey
+# Date: 4/23/2024
+
+#gather parameters from command line
+strExternalIP=$1
+strTicketID=$2
+
+#debugstatements
+ echo ${strExternalIP} ${strTicketID}
