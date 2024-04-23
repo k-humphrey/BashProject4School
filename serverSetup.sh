14d7ccd7396c8a32472e94255b566fb7bf9d4070
Kayla Humphrey <kl1humphrey@gmail.com>
2024-04-23 23:59:00 +0000

    created directory and log file

--- serverSetup.sh
+++ serverSetup.sh
@@ -3,9 +3,16 @@
 # Creator: Kayla Humphrey
 # Date: 4/23/2024
 
-#gather parameters from command line
+# gather parameters from command line
 strExternalIP=$1
 strTicketID=$2
 
-#debugstatements
- echo ${strExternalIP} ${strTicketID}
+#create a directory for the logs, or just go into it if it exists and create log file
+mkdir -p configurationLogs && cd configurationLogs
+touch ${strTicketID}.log
+
+# debugstatements
+# echo ${strExternalIP} ${strTicketID}
+
+# Now, we have to use the webservice to get ticket info
+
