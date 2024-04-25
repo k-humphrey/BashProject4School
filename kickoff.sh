ca38ff87eebaba6e5d43881a3a2cec666afee2fc
Kayla Humphrey <kl1humphrey@gmail.com>
2024-04-25 20:57:01 +0000

    Updated kickoff.sh to allow ssh connection to run serverSetup.sh remotely

--- kickoff.sh
+++ kickoff.sh
@@ -8,6 +8,16 @@ strIP=$1
 strTicketID=$2
 strUsername=$3
 
-echo ${strIP}
-echo ${strTicketID}
-echo ${strUsername}
+#echo ${strIP}
+#echo ${strTicketID}
+#echo ${strUsername}
+
+#Given ssh stored in this directory and named this way
+ssh-add .ssh/gcpserver
+
+#Using scp, copy serverSetup.sh from current directory to home directory on remote server
+scp -i .ssh/gcpserver serverSetup.sh "${strUsername}"@"${strIP}":/home/"${strUsername}"
+
+#give serverSetup.sh execution rights and run it on the remote server
+ssh ${strUsername}@${strIP} "chmod 755 serverSetup.sh"
+ssh ${strUsername}@${strIP} "./serverSetup.sh ${strIP} ${strUsername}"
